fee21bebe782c07bc6c58e2fc878181010b5f079
AndreaDev14 <andreatester17@gmail.com>
2022-10-22 16:20:51 -0300

    project huddle landing page

---
+++ README.md
@@ -0,0 +1,31 @@
+# Frontend Mentor - Huddle landing page with curved sections solution
+
+### Link
+
+- Live Site URL:
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Sass - gulp
+- JavaScript
+- [SweetAlert](https://sweetalert2.github.io/) 
+
+
+
+
+
+- Frontend Mentor - [@AndreaDev14](https://www.frontendmentor.io/profile/AndreaDev14)
+
+
---
+++ js/app.js
@@ -0,0 +1,56 @@
+
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const formField = document.querySelector('.form__field')
+
+
+
+
+eventListener();
+
+function eventListener() {
+    input.addEventListener('blur', emailValidation);
+    form.addEventListener('submit', getSubscription);
+
+}
+
+function getSubscription(e) {
+    e.preventDefault();
+
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'You have subscribed successfully',
+        showConfirmButton: false,
+        timer: 1800
+      })
+
+      input.classList.remove('footer__form__input-correct');
+      form.reset();
+    
+}
+
+function emailValidation(e) {
+    
+const  mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+if(e.target.value.match(mailformat)) {
+    formField.removeChild(formField.lastChild);
+    
+    input.classList.remove('footer__form__input-error');
+    input.classList.add('footer__form__input-correct');
+
+} else{
+    input.classList.remove('footer__form__input-correct');
+    input.classList.add('footer__form__input-error');
+    
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('footer__error')
+    paragraph.innerHTML = 'Check your email please';
+
+    formField.appendChild(paragraph);
+}
+}
+
+
+    
